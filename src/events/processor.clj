df8c4f865ee32064945ff5b2aafe93e1c5f41abd
JR Richardson <jr@codeclimate.com>
2015-04-13 13:15:51 -0600

    De-dup events by id

--- src/events/processor.clj
+++ src/events/processor.clj
@@ -1,17 +1,34 @@
 (ns events.processor)
 
+(defn- get-timestamp [user-state]
+  (get (meta user-state) :timestamp 0))
+
+(defn- duplicated-event? [state event-data]
+  (get-in (meta @state) [:dups (:id event-data)]))
+
 (defn- timestamped-merge [timestamp state data]
-  (with-meta (merge state data)
+  (with-meta (merge state data) ; Add timestamp to user data
     {:timestamp timestamp}))
 
-(defn- merge-state [state user_id data timestamp]
-  (if (> timestamp (get (meta (state user_id)) :timestamp 0))
-    (update-in state [user_id] (partial timestamped-merge timestamp) data)
-    state))
+(defn- merge-user-state [state {:keys [user-id data timestamp]}]
+  (when (> timestamp (get-timestamp (state user-id)))
+    (update-in state
+               [user-id]
+               (partial timestamped-merge timestamp)
+               data)))
 
-(defn process [{:keys [user_id data timestamp]} state]
-  (swap! state merge-state user_id data timestamp))
+(defn- merge-state [state event-data]
+  (with-meta (or (merge-user-state state event-data)
+                 state)
+    (update-in (meta state) [:dups] conj (:id event-data)))) ; Add event id to dups set
 
-(defn make-processor [state]
-  (fn [event]
-    (process event state)))
+(defn- process [event-data state]
+  (when-not (duplicated-event? state event-data)
+    (swap! state merge-state event-data)))
+
+(defn make-processor
+  ([state] (make-processor state #{})) ; Dups default to empty set
+  ([state dups]
+   (swap! state with-meta {:dups dups}) ; Associate dups meta data
+   (fn [event]
+     (process event state))))
--- test/events/processor_test.clj
+++ test/events/processor_test.clj
@@ -4,33 +4,52 @@
 
 (def event
   {
-   ;:id "c52543d8-da03-11e4-8e29-c5dc2fe5941b",
+   :id "c52543d8-da03-11e4-8e29-c5dc2fe5941b",
    :type "attributes"
-   :user_id "2352"
+   :user-id "2352"
    :data {"name" "Bill", "email" "bill@gmail.com"}
    :timestamp 1428067050
    })
 
+(defn- new-event [event-data]
+  (merge event
+         {:id (str (java.util.UUID/randomUUID))}
+         event-data))
+
 (deftest process-test
-  (let [state (atom {})
-        process-event (make-processor state)]
-    (testing "stores event data by user_id"
+  (testing "stores event data by user_id"
+    (let [state (atom {})
+          process-event (make-processor state)]
       (process-event event)
       (is (= @state
-             {"2352" {"name" "Bill", "email" "bill@gmail.com"}})))
-    (testing "stores data with latest timestamp"
-      (let [early-event (merge event {:data {"name" "Graham"}
-                                      :timestamp (dec (:timestamp event))})
-            late-event (merge event {:data {"name" "Bob"}
-                                     :timestamp (inc (:timestamp event))})]
-        (process-event event)
-        (is (= @state
-               {"2352" (:data event)}))
-        (process-event early-event)
-        (is (= @state
-               {"2352" (:data event)}))
-        (process-event late-event)
-        (is (= @state
-               {"2352" (merge (:data event)
-                              (:data late-event))}))))
-    ))
+             {"2352" {"name" "Bill", "email" "bill@gmail.com"}}))))
+
+  (testing "stores data with latest timestamp"
+    (let [state (atom {})
+          process-event (make-processor state)
+          early-event (new-event {:data {"name" "Graham"}
+                                  :timestamp (dec (:timestamp event))})
+          late-event (new-event {:data {"name" "Bob"}
+                                 :timestamp (inc (:timestamp event))})]
+      (process-event event)
+      (is (= @state
+             {"2352" (:data event)}))
+      (process-event early-event)
+      (is (= @state
+             {"2352" (:data event)}))
+      (process-event late-event)
+      (is (= @state
+             {"2352" (merge (:data event)
+                            (:data late-event))}))))
+
+  (testing "de-dupes by id"
+    (let [state (atom {})
+          process-event (make-processor state)
+          duped-event (merge event {:data {"foo" "bar"}
+                                    :timestamp (inc (:timestamp event))})]
+      (process-event event)
+      (process-event duped-event)
+      (is (= @state
+             {"2352" (:data event)}))))
+
+  )
